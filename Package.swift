206a25d83cf925a6a11ca2cc2fa38f8553aa5ce1
pushwoosh <pushwoosh@Pushwooshs-Mac-mini-001.local>
2024-09-23 21:15:50 +0700

    Release 6.8.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Pushwoosh",
-            url: "https://github.com/bormog/pushwoosh-ios-sdk/releases/download/6.7.5/PushwooshFramework.xcframework.zip",
-            checksum: "ae2c36941dedf01a8ad58bfdd0f909a59a91b246de64a74ede2055339212b250"
+            url: "https://github.com/bormog/pushwoosh-ios-sdk/releases/download/6.8.0/PushwooshFramework.xcframework.zip",
+            checksum: "72601c52848ac011db91aa8a7d5a223fcf20391211fba3d9dd49d8f6e7e1500e"
         )
     ]
 )
